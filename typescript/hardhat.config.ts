795f88ccf95719a16ccb7b5b0f65e6c4c3d1d47b
solenemep <solene.pettier@gmail.com>
2025-05-06 13:05:35 +0200

    ci: remove anvil url

--- typescript/hardhat.config.ts
+++ typescript/hardhat.config.ts
@@ -16,8 +16,6 @@ import "./scripts/deployments/pause/tasks/unpause.ts";
 import dotenv from "dotenv";
 dotenv.config();
 
-const privateKey = process.env.PRIVATE_KEY || "";
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
@@ -33,28 +31,22 @@ const config: HardhatUserConfig = {
     localhost: {
       chainId: 31337,
       allowUnlimitedContractSize: true,
-      url: "http://anvil:8545",
-      // accounts: [privateKey],
     },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       chainId: 11155111,
-      // accounts: [privateKey],
     },
     ethereum: {
       url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       chainId: 1,
-      // accounts: [privateKey],
     },
     moonbaseAlpha: {
       url: "https://rpc.api.moonbase.moonbeam.network",
       chainId: 1287,
-      // accounts: [privateKey],
     },
     polygonMumbai: {
       url: "https://rpc.ankr.com/polygon_mumbai",
       chainId: 80001,
-      // accounts: [privateKey],
     },
   },
   etherscan: {
